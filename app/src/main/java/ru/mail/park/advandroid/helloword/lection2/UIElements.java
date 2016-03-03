b1b40e8515414e6be9e7ed25292a05d447937318
Vladimir Letyago <vlad.letyago@gmail.com>
2016-03-03 16:05:18 +0300

    Fix Spinner

--- app/src/main/java/ru/mail/park/advandroid/helloword/lection2/UIElements.java
+++ app/src/main/java/ru/mail/park/advandroid/helloword/lection2/UIElements.java
@@ -3,7 +3,10 @@ package ru.mail.park.advandroid.helloword.lection2;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class UIElements extends Activity {
@@ -15,7 +18,29 @@ public class UIElements extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ui_elements);
 
+		String[] data = {"one", "two", "three", "four", "five"};
+
 		pushButton = (Button)findViewById(R.id.push_button);
+		Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, data);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner.setAdapter(adapter);
+		spinner.setPrompt("Title");
+		spinner.setSelection(2);
+
+		spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+											  @Override
+											  public void onItemSelected(AdapterView<?> parent, View view,
+																		 int position, long id) {
+												  Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+											  }
+
+											  @Override
+											  public void onNothingSelected(AdapterView<?> parent) {
+
+											  }
+										  });
 
 		setListeners();
 	}
